08c104f6caf572961c46109e2e9bf537a4efdc39
lauraszyms <lauraszyms@gmail.com>
2018-02-06 18:46:06 -0500

    Added main goals index and show

--- app/controllers/main_goals_controller.rb
+++ app/controllers/main_goals_controller.rb
@@ -1,2 +1,11 @@
 class MainGoalsController < ApplicationController
+
+  def index
+   @main_goals = MainGoal.all
+  end
+
+  def show
+   @main_goal = MainGoal.find_by(:id => params[:id])
+   @user = current_user
+  end
 end
