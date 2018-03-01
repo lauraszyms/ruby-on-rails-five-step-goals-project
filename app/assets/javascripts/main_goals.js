17fa98a4336dd338025c2fcc9ea8378cf808fc8f
lauraszyms <lauraszyms@gmail.com>
2018-03-01 15:32:23 -0500

    harcoded goalstep link

--- app/assets/javascripts/main_goals.js
+++ app/assets/javascripts/main_goals.js
@@ -3,12 +3,11 @@ $(document).ready(function(){
 
 
     $.get(this.href).then(function(json){
-
       var $ol = $("div.goal_steps ol")
       $ol.html("")
       json.forEach(function(gs){
-       let newGoal = new GoalStep(gs.id, gs.title, gs.summary, gs.status, gs.main_goal)
-      $ol.append('<li><a class="load_goal_step" href="' + newGoal.stepLink + '">' + newGoal.title + '</a></li>');
+       let newGoal = new GoalStep(gs.id, gs.title, gs.summary, gs.status, gs.main_goal.id)
+      $ol.append('<li><a class="load_goal_step" href="http://localhost:3000/main_goals/1/goal_steps/1">' + newGoal.title + '</a></li>');
       })
     })
      e.preventDefault();
@@ -16,21 +15,21 @@ $(document).ready(function(){
 
 $(document).on('click', '.load_goal_step', function (e) {
     alert("It's Working!")
-    e.preventDefault();
+    // e.preventDefault();
   })
 
   class GoalStep {
-   constructor (id, title, summary, status, main_goal) {
+   constructor (id, title, summary, status, main_goal_id) {
     this.id = id;
     this.title = title;
     this.summary = summary;
     this.status = status;
-    this.main_goal = main_goal;
-   }
+    this.main_goal_id = main_goal_id;
+  }
 
-   stepLink(){
-     return `main_goal_goal_step_path(${this.id})`;
-   };
+   stepLink() {
+    return 'http://localhost:3000/main_goals/1/goal_step/1'
+    }
   };
 
 
