c4c464d08c44a3441b2eb1c122e82dbfbdd1a678
lauraszyms <lauraszyms@gmail.com>
2018-02-28 21:47:35 -0500

    refactoring maingoal goal step get request into json

--- app/assets/javascripts/main_goals.js
+++ app/assets/javascripts/main_goals.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
   $("a.load_goal_steps").on("click", function(e) {
 
 
-    $.ajax({
-      method: "GET",
-      url: this.href
-    }).done(function(resp){
-      $("div.goal_steps").append(resp)
+    // $.ajax({
+    //   method: "GET",
+    //   url: this.href
+    // }).done(function(resp){
+    //   $("div.goal_steps").html(resp)
+    // })
+
+    $.get(this.href).success(function(json){
+      $(div.goal_steps ol).html("")
     })
 
 
