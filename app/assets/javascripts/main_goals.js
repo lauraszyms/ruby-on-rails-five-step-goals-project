bc3b2510a628e08f0a4bb84f1f4bd11196efaf22
lauraszyms <lauraszyms@gmail.com>
2018-03-01 11:43:30 -0500

    added goalstep model object to main goal js

--- app/assets/javascripts/main_goals.js
+++ app/assets/javascripts/main_goals.js
@@ -3,14 +3,30 @@ $(document).ready(function(){
 
 
     $.get(this.href).then(function(json){
+
       var $ol = $("div.goal_steps ol")
       $ol.html("")
       json.forEach(function(goal_step){
-        $ol.append("<li>" + goal_step.title + "</li>")
+      $ol.append("<li>" + goal_step.stepLink + "</li>")
       })
     })
 
 
     e.preventDefault();
   })
-})
+
+  class GoalStep {
+   constructor (id, summary, status, main_goal_id) {
+    this.id = id;
+    this.summary = summary;
+    this.status = status;
+    this.main_goal_id = main_goal_id;
+   }
+
+   stepLink = function(){
+     return `link_to ${this.name}, main_goal_goal_step_path(${this.id});`
+   };
+  };
+
+
+});
