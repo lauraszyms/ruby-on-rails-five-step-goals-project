8d693e4f796e84346a5865308898e0388f79fcd2
lauraszyms <lauraszyms@gmail.com>
2018-02-28 20:32:47 -0500

    Added AJAX get request

--- app/assets/javascripts/main_goals.js
+++ app/assets/javascripts/main_goals.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
   $("a.load_goal_steps").on("click", function(e) {
+    $.ajax({
+      method: "GET"
+      url:
+    }).done(function(data){
+      console.log(data)
+    })
+
     alert("It's working!")
     e.preventDefault();
   })
