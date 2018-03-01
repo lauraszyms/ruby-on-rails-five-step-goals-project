80455431dac855ee13efe108d2720ccbb19f4d91
lauraszyms <lauraszyms@gmail.com>
2018-02-28 23:16:24 -0500

    deleted commented out code

--- app/assets/javascripts/main_goals.js
+++ app/assets/javascripts/main_goals.js
@@ -2,13 +2,6 @@ $(document).ready(function(){
   $("a.load_goal_steps").on("click", function(e) {
 
 
-    // $.ajax({
-    //   method: "GET",
-    //   url: this.href
-    // }).done(function(resp){
-    //   $("div.goal_steps").html(resp)
-    // })
-
     $.get(this.href).then(function(json){
       var $ol = $("div.goal_steps ol")
       $ol.html("")
