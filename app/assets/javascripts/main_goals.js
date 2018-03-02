bbbf95e3e1eb627afbfd7fbdb4e8b303379a22bf
lauraszyms <lauraszyms@gmail.com>
2018-03-01 20:24:02 -0500

    add form field to main_goals

--- app/assets/javascripts/main_goals.js
+++ app/assets/javascripts/main_goals.js
@@ -3,8 +3,11 @@ $(document).ready(function(){
 
 
     $.get(this.href).then(function(json){
-      var $ol = $("div.goal_steps ol")
+      let $ol = $("div.goal_steps ol")
+      let $p = $("div.goal_steps p")
+      let $href = $("a.load_goal_steps")
       $ol.html("")
+      $href.hide()
       json.forEach(function(gs){
        let newGoal = new GoalStep(gs.id, gs.title, gs.summary, gs.status, gs.main_goal.id)
       $ol.append('<li><a class="load_goal_step" href="http://localhost:3000/main_goals/1/goal_steps/1">' + newGoal.title + '</a></li>');
@@ -22,7 +25,6 @@ $(document).on('click', '.load_goal_step', function (e) {
     $h3.html("")
     $p.html("")
     let newStep = new GoalStep(json.id, json.title, json.summary, json.status, json.main_goal.id)
-    $h1.html("")
     $h1.append(newStep.title)
     $h3.append(newStep.status)
     $p.append(newStep.summary)
