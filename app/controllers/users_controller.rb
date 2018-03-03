d4ba38ff5543f254374cda378f14be4a4475137a
lauraszyms <lauraszyms@gmail.com>
2018-03-02 21:53:55 -0500

    refactor style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,6 @@ class UsersController < ApplicationController
     @user = current_user
     @overdue = current_user.main_goals.in_progress.overdue
     @completed = current_user.main_goals.completed
+    @in_progress = current_user.main_goals.in_progress
   end
 end
